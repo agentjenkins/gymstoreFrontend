68467ac1900fe7ba6f605956aa305270753eb450
marta <blomster75@gmail.com>
2019-03-29 16:51:04 +0100

    inital superbar commit

---
+++ src/components/ItemList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{item}}</h1>
+    <h2>{{name}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'item',
+  data () {
+    return {
+      item: 'Bars',
+      name: 'superBar'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
