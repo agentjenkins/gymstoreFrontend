4b4417fbff5fd11956b3f81280666a34bcd36fd9
Thomas <thomas@sirister.se>
2019-03-31 14:39:50 +0200

    front end changes, added some styling

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.background{
+  color: grey;
+}
 </style>
--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,8 +1,9 @@
+@import "compass/css3";
 <template>
-  <div>
-    <h1>Item List</h1>
-    <ul id='itemList'>
-      <li v-for="(item) in items" v-bind:key="item.id"><button v-on:click="buyItem(item)">Buy {{item.name}}</button></li>
+  <div class="center" >
+    <h1>Buy some Bars and Snacks</h1>
+    <ul id='itemList' class="flex-container">
+      <li class="flex-item" v-for="(item) in items" v-bind:key="item.id"><button v-on:click="buyItem(item)"> <img class="flex-item" src="../assets/nocco.png"> Buy {{item.name}}</button></li>
     </ul>
   </div>
 </template>
@@ -32,4 +33,41 @@ export default {
 </script>
 
 <style scoped>
+ .center {
+    margin: auto;
+    width: 30%;
+    border: 3px solid lightblue;
+    padding: 10px;
+  }
+
+.flex-container {
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 10;
+  margin: 10;
+  align: center;
+  list-style: none;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-flow:  wrap;
+  justify-content: flex-start;
+
+}
+
+.flex-item {
+  padding: 5px;
+  width: 75px;
+  margin-top: 10px;
+  flex-basis: 100px;
+
+  line-height: 150px;
+  color: white;
+  font-weight: bold;
+  font-size: 3em;
+  text-align: center;
+}
+
 </style>
